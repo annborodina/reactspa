068dfafaf4fc62aa05aa4f51d05644427d4f4620
Anna Borodina <ann.borodina05@gmail.com>
2025-05-15 16:31:45 +0300

    Initial commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+function Footer(){
+    return(
+        <footer className = "page-footer light-green darken-2">
+          <div className = "footer-copyright">
+            <div className = "container">
+            © Анна, Татьяна, Мария
+            <a className = "grey-text text-lighten-4 right" href="#!">Repository</a>
+            </div>
+          </div>
+        </footer>
+    )
+}
+
+export {Footer};
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const API_KEY = "6d8b0e14"
+
+const API_URL = `https://www.themealdb.com/api/json/v1/${API_KEY}/`
+
+export {API_URL}
